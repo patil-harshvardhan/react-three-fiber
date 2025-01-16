5ff5a478b33fb71372518b6743cc01a65363087d
Harshvardhan Patil <harshvardhanpatil@Harshvardhans-MacBook-Air.local>
2025-01-16 23:21:40 +0000

    github pages

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { useRef } from "react";
-import { Mesh } from "three";
 
 
 function Cube() {
